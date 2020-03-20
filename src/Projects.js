769abe8b14a3fdc8dc4eec23a0e64da74425c6f6
4yerik <4yerik@gmail.com>
2020-03-19 22:38:37 -0400

    names

--- src/Projects.js
+++ src/Projects.js
@@ -8,15 +8,18 @@ const details = [
   {
     image: "https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/virtual_console_nintendo_3ds_7/SI_3DSVC_SuperMarioBros.jpg",
     resolver: "(parent, args, context, info) => data",
-    tricks: "pagination"
+    tricks: "pagination",
+    name: "super mario bros"
   },
   {
     image: "https://www.thesprucepets.com/thmb/LPnj-N-AcGdZRvf7E6y_Hsf2vqo=/4089x2300/smart/filters:no_upscale()/close-up-of-a-hamster-eating-groundnut-635096689-5c525f88c9e77c0001d7c1fa.jpg",
     resolver: "(parent, args, context, info) => data",
-    tricks: "pagination"
+    tricks: "pagination",
+    name: "identity"
   },
   {
-    image: "https://github.com/4yerik/images/blob/master/projects/website.PNG?raw=true"
+    image: "https://github.com/4yerik/images/blob/master/projects/website.PNG?raw=true",
+    name: "nothing"
   }
 
 ];
@@ -27,7 +30,7 @@ const Projects = () => (
       <div className="project">
         <Carousel useKeyboardArrows={true} dynamicHeight={true} showStatus={false} infiniteLoop={true}>
                 <div>
-                    <p className="legend">Legend 1</p>
+                    <p className="legend">{detail.name}</p>
                     <img className="project-image" src={detail.image}></img>
                 </div>
                 <div>
